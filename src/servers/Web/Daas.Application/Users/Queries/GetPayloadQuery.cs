b2c13a2d40f4ff1f0530f493d80400ed54ad9afc
Akash C <121154000+akashc15@users.noreply.github.com>
2026-02-03 20:40:56 +0530

    Added the Query and QueryHandler

---
+++ src/servers/Web/Daas.Application/Users/Queries/GetPayloadQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using Daas.Domain.Entities;
+using Daas.Application.DTO.RequestDTO;
+namespace Daas.Application.Users.Queries;
+
+
+public record GetPayloadQuery( RequestModel[]  sus ):IRequest<ResultModel[]>;
+ 
---
+++ src/servers/Web/Daas.Application/Users/Queries/GetPayloadQueryHandlers.cs
@@ -0,0 +1,40 @@
+﻿using Daas.Domain.Entities;
+
+using MediatR;
+using Daas.Domain.Entities;
+using Microsoft.VisualBasic.FileIO;
+
+namespace Daas.Application.Users.Queries;
+
+public class GetPayloadQueryHandlers : IRequestHandler<GetPayloadQuery, ResultModel[]>
+{
+    public Task<ResultModel[]> Handle(GetPayloadQuery request, CancellationToken cancellationToken)
+    {
+        var one="";
+        var two="";
+        var three="";
+        var four="";
+
+            one = request.sus[0].fieldType;
+            two = request.sus[0].fieldValue;
+            three = request.sus[1].fieldType;
+            four = request.sus[1].fieldValue;
+        
+
+
+        ResultModel[] P=new ResultModel[3];
+        P[0] = new ResultModel();
+        P[1] = new ResultModel();
+        P[2] = new ResultModel();
+        P[0].columnValue = "1";
+        P[1].columnValue = "2";
+        P[2].columnValue = "3";
+        P[0].fieldValue = "Akash";
+        P[1].fieldValue = "Krishna";
+        P[2].fieldValue = "Mohan";
+        
+        
+        return Task.FromResult(P);
+
+    }
+}
